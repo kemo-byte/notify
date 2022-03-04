5031e40533d4ab5ab47751243b94863b40afcc89
kemo-byte <kamalkafi12@gmail.com>
2022-03-04 21:11:25 +0200

    first commit for notification app using pusher channel

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Events\MyEvent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/test', function() {
+    event(new MyEvent('{"message":hello world"}'));
+    return 'Event has been sent';
+});
